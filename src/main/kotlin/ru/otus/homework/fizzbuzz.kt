8b7c066869612baa48360368a97acd9cc2eb432b
maxdgladkov <maxdgladkov@gmail.com>
2026-03-20 23:23:52 +0300

    fizzbuzz

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,20 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val resArr = Array(n) {""}
+
+    if (n == 0) {
+        return resArr
+    }
+
+    for (i in 0..n) {
+        when {
+            ((i % 5 == 0) && (i % 3 == 0)) || (i == 0) -> resArr[i] = "FizzBuzz"
+            (i % 3 == 0) -> resArr[i] = "Fizz"
+            (i % 5 == 0) -> resArr[i] = "Buzz"
+            else -> resArr[i] = "$i"
+        }
+    }
+
+    return resArr
 }
