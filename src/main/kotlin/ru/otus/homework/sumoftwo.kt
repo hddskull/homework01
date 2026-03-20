cbbae9614185080a9b43b2241e0cb6715c9ef0e2
maxdgladkov <maxdgladkov@gmail.com>
2026-03-20 23:50:59 +0300

    sumOfTwo debug code removed

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,22 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    for (i in 0..<numbers.size) {
+
+        for (j in 0..<numbers.size) {
+
+            if (i == j) {
+                continue
+            }
+
+            if (numbers[i] + numbers[j] == target) {
+                val arr = IntArray(2)
+                arr[0] = i
+                arr[1] = j
+                return arr
+            }
+        }
+    }
+
+    throw IllegalArgumentException("no such numbers")
 }
